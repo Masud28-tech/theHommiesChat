718d31840f20928beccda246c4c79a74b0aa6f76
Masud28-Tech <masud.tech28@gmail.com>
2022-09-13 20:16:31 +0530

    access-control-allow-origin issue fix try 2

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/api/message', messagesRoutes);
 
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://hommies-chat.netlify.app");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
